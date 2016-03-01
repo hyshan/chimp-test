5aaa94663c0b051ba695454ce04a3d6fa7091500
Hsiu-Yuan Shan <hyshan@cht.com.tw>
2016-03-01 11:18:12 +0800

    initial coomit

---
+++ features/search.feature
@@ -0,0 +1,34 @@
+﻿Feature: NTA XYZ system EAC108E CRUD Function
+
+  As a NTA User
+  I want to login NTA XYZ system
+  and Create/Read/Update/Delete EAC108E Business Enyity
+
+@watch
+Scenario: Login EAC108E
+  Given I have visited NTA XYZ
+  When I press Enter Key
+  Then I see "作業功能範例" page
+
+@watch
+Scenario: Create EAC108E Business Enyity
+  Given I have visited NTA XYZ EAC108E
+  When I press create button
+  And wait for user interaction finish
+  Then I see "新增成功。"
+
+@watch
+Scenario: Search EAC108E Business Entity after creation
+  Given I have visited NTA XYZ EAC108E
+  When I press search button
+  Then I see "查詢成功。"
+  
+@watch
+Scenario: Update EAC108E Business Entity
+  When I press update button
+  Then I see "更新成功。"  
+  
+@watch
+Scenario: Delete EAC108E Business Entity
+  When I press delete button
+  Then I see "刪除成功。"  
---
+++ features/support/step_defs.js
@@ -0,0 +1,56 @@
+﻿module.exports = function() {
+
+  this.Given(/^I have visited NTA XYZ$/, function () {
+    browser.url('http://192.168.30.95/xyz/');
+  });
+
+  this.When(/^I press Enter Key$/, function () {
+    browser.keys(['Enter']);
+  });
+
+  this.Then(/^I see "([^"]*)" page$/, function (arg1) {
+    expect(browser.getText('div[style="background-color: yellow;"] h1')).toEqual(arg1);
+  });
+
+  this.Given(/^I have visited NTA XYZ EAC108E$/, function () {
+     browser.url('http://192.168.30.95/xyz/EAC108E/');
+  });
+
+  this.When(/^I press create button$/, function () {
+    browser.setValue('input[name="ped"]', "123");
+    browser.click('.btn-primary[data-ng-click="create()"]');
+  });
+
+  this.When(/^wait for user interaction finish$/, {timeout: 90000}, function () {
+    browser.waitForVisible('button[data-bb-handler="確定"]', 90000);
+    browser.click('button[data-bb-handler="確定"]');
+  });
+
+  this.Then(/^I see "([^"]*)"$/, {timeout: 90000}, function (arg1) {
+    
+    browser.waitForVisible('#alertArea span h4', 90000)
+    browser.waitUntil(function(){
+       if ( browser.getText('#alertArea span h4') == arg1) {
+           return true;
+       } else {
+           return false;
+       }  
+    }, 90000);
+  });
+
+  this.When(/^I press search button$/, function () {
+    browser.click('.btn-primary[data-ng-click="query()"]');
+  });
+  
+  this.When(/^I press update button$/, function () {
+    browser.waitForVisible('div.blockUI.blockOverlay', 5000, true);
+    browser.setValue('input[name="nam"]', "567");
+    browser.click('.btn-primary[data-ng-click="save()"]');
+  });
+  
+  this.When(/^I press delete button$/, function () {
+    browser.waitForVisible('div.blockUI.blockOverlay', 5000, true);
+    browser.click('.btn-primary[data-ng-click="remove()"]');
+  });
+
+}
